417413c64d9216d66495c4ca4af6f496ec14258e
Nafis Sadat <sadatnfs@uw.edu>
2018-08-05 11:23:20 -0700

    Fixing install_requires and switch to find_packages

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import numpy
+VERSION = 0.1
 
 setup(
     name='PyMB',
-    version='0.1',
-    packages=['', 'PyMB.magic'],
+    version=VERSION,
+#    packages=['', 'PyMB.magic'],
+    packages=find_packages(),
     url='https://github.com/kforeman/PyMB',
     license='GNU General Public License v2.0',
     author='Kyle Foreman',
     author_email='kforeman <at> post <dot> harvard <dot> edu',
     description='Python Model Builder',
     include_dirs=[
+        numpy.get_include(),
+    ],
+    install_requires=[
         'numpy', 'rpy2', 'scipy',
     ]
 )
