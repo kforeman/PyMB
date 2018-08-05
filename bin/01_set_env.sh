fda949ca037bf822a1a46a6bc66253f94e21b3a6
Nafis Sadat <sadatnfs@uw.edu>
2018-08-05 16:37:30 -0700

    remove intel packages

--- bin/01_set_env.sh
+++ bin/01_set_env.sh
@@ -1,8 +1,8 @@
 
 ## Install numpy, scipy and and dependencies on Python side
-conda install -c intel -y numpy scipy ipython
-conda install -c conda-forge -y scikit-sparse libiconv libxml2 lxml
+conda install -y numpy scipy libiconv libxml2 lxml
+conda install -c conda-forge -y scikit-sparse
 
 ## Install R and rpy2
-conda install -c r -y r-base=3.5.0 
+conda install -c r -y r-base=3.5.0
 pip install rpy2
