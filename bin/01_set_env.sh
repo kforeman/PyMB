e5f9f13183fcbc2d29265a26573e396aa12ad13b
Nafis Sadat <sadatnfs@uw.edu>
2018-08-05 16:55:22 -0700

    Move conda installation to 01 shell

--- bin/01_set_env.sh
+++ bin/01_set_env.sh
@@ -1,4 +1,9 @@
 
+## Install Miniconda3
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+
 ## Install numpy, scipy and and dependencies on Python side
 conda install -y numpy scipy libiconv libxml2 lxml
 conda install -c conda-forge -y scikit-sparse
