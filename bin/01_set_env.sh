c6f0e375ca78fa994d6cf8b48ee97f5072587ad1
Nafis Sadat <sadatnfs@uw.edu>
2018-08-05 16:33:01 -0700

    Add binaries for dependency installation

--- bin/01_set_env.sh
+++ bin/01_set_env.sh
@@ -1,2 +1,8 @@
-which -a python
-python -c 'import numpy as np; np.show_config()'
+
+## Install numpy, scipy and and dependencies on Python side
+conda install -c intel -y numpy scipy ipython
+conda install -c conda-forge -y scikit-sparse libiconv libxml2 lxml
+
+## Install R and rpy2
+conda install -c r -y r-base=3.5.0 
+pip install rpy2
