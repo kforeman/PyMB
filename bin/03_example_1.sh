554ac05a3c563647f878b1acbf5f74459877d331
Nafis Sadat <sadatnfs@uw.edu>
2018-08-05 18:02:28 -0700

    try install again ugh

--- bin/03_example_1.sh
+++ bin/03_example_1.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
+ls ~/
 export PATH="$HOME/miniconda/bin:$PATH"
 which -a python
 which -a R
 
-R -e 'library(TMB); runExample("randomregression");
+R -e 'library(TMB); runExample("randomregression");'
 
