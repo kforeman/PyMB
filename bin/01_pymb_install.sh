c4ec93e25c83ea5cd05ccb80e01c67b0afedd248
Nafis Sadat <sadatnfs@uw.edu>
2018-08-05 19:26:00 -0700

    - Adding build status on README - Squash build and test in a single stage - Move the R package installation in the install block

---
+++ bin/01_pymb_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
+## Install PyMB
+cd ..
+python setup.py install
---
+++ bin/02_pymb_linreg.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
+## Run linreg example
+python Examples/linreg.py
