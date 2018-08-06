4cbea531fa99658051ef268ff034ccc01d817f56
Nafis Sadat <sadatnfs@uw.edu>
2018-08-05 18:40:50 -0700

    Move back conda to before inst

--- bin/01_set_env.sh
+++ bin/01_set_env.sh
@@ -2,24 +2,24 @@
 set -e # Exit with nonzero exit code if anything fails
 
 ## Install Miniconda3
-sudo apt-get install -y \
-    wget \
-    curl \
-    bzip2 \
-    ca-certificates \
-    libglib2.0-0 \
-    libxext6 \
-    libsm6 \
-    libxrender1 \
-    git \
-    gcc \
-    g++ \
-    libreadline-dev \
-    tar \
-    build-essential
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
-bash miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
+# sudo apt-get install -y \
+#     wget \
+#     curl \
+#     bzip2 \
+#     ca-certificates \
+#     libglib2.0-0 \
+#     libxext6 \
+#     libsm6 \
+#     libxrender1 \
+#     git \
+#     gcc \
+#     g++ \
+#     libreadline-dev \
+#     tar \
+#     build-essential
+# wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+# bash miniconda.sh -b -p $HOME/miniconda
+# export PATH="$HOME/miniconda/bin:$PATH"
 
 ## Install numpy, scipy and and dependencies on Python side
 # conda install -c conda-forge  -y numpy scipy libiconv libxml2 lxml scikit-sparse
