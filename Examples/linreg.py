9ed1e4c95b2fea6c739957abc74f66a5a901a3ea
Nafis Sadat <sadatnfs@uw.edu>
2018-08-05 21:24:39 -0700

    let's build TMB from source and not include the Eigen flag

--- Examples/linreg.py
+++ Examples/linreg.py
@@ -33,8 +33,8 @@ Type objective_function<Type>::operator() (){
 
 # Get the necessary paths to compile TMB code
 tmbinclude = ro.r('paste0(find.package("TMB"), "/include")')[0]
-eigeninclude = ro.r('paste0(find.package("RcppEigen"), "/include")')[0]
-tmbinclude = tmbinclude + ' -I' + eigeninclude
+# eigeninclude = ro.r('paste0(find.package("RcppEigen"), "/include")')[0]
+# tmbinclude = tmbinclude + ' -I' + eigeninclude
 rinclude = rin.R_HOME + "/include"
 rlib = rin.R_HOME + "/lib"
 
