903b431a2425e14b0365c96022f27d3d57c4df20
Nafis Sadat <sadatnfs@uw.edu>
2018-08-05 17:45:30 -0700

    Reset path thing?

--- bin/02_install_TMB.sh
+++ bin/02_install_TMB.sh
@@ -3,13 +3,8 @@
 set -e # Exit with nonzero exit code if anything fails
 
 ## Check R
-which -a python
-echo $PATH
 export PATH="$HOME/miniconda/bin:$PATH"
-ls ~/
-
-which -a R
-R -e 'sessionInfo()'
+which -a python
 
 # R -e 'install.packages("Matrix",  repos="http://cran.us.r-project.org", dependencies=T)'
 # R -e 'install.packages("TMB",  repos="http://cran.us.r-project.org", dependencies=T)'
