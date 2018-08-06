b3efcacb4622bae77daa1c89e006faa7372d6b59
Nafis Sadat <sadatnfs@uw.edu>
2018-08-05 21:35:21 -0700

    Run example inline

--- bin/01_pymb_install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e # Exit with nonzero exit code if anything fails
-
-## Install PyMB
-cd ../PyMB
-python setup.py install
--- bin/02_pymb_linreg.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e # Exit with nonzero exit code if anything fails
-
-## Run linreg example
-python Examples/linreg.py
